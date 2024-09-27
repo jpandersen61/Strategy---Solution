4005b887f7d6ada5c2895a41f721542de122129f
Zealand-Jean <55512813+Zealand-Jean@users.noreply.github.com>
2024-09-27 09:57:07 +0200

    Add project files.

---
+++ Observer - Start/PriceStrategies.cs
@@ -0,0 +1,33 @@
+﻿using PizzaApp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaApp
+{
+    public class EveningPriceStrategy : IPriceStrategy
+    {
+        public  double CalculatePrice(IMenu menu)
+        {
+            return menu.MenuPrice;
+        }
+    }
+
+    public class LunchPriceStrategy : IPriceStrategy
+    {
+       
+
+        public  double CalculatePrice(IMenu menu)
+        {
+            return menu.MenuPrice*0.8;
+        }
+    }
+
+    public class NightPriceStrategy : IPriceStrategy
+    {
+        public  double CalculatePrice(IMenu menu)
+        {
+            return menu.MenuPrice * 1.1;
+        }
+    }
+}
